92caf9d7f5b6019fa3049766856c3b89290621c2
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-02-13 16:12:06 -0500

    GOT PARALLAX TO WORK!

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,41 +1,4 @@
 
-// Name animation code from https://tobiasahlin.com
-anime.timeline({loop: true})
-  .add({
-    targets: '.headingAnimation .line',
-    opacity: [0.5,1],
-    scaleX: [0, 1],
-    easing: "easeInOutExpo",
-    duration: 1000
-  }).add({
-    targets: '.headingAnimation .line',
-    duration: 1100,
-    easing: "easeOutExpo",
-    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
-  }).add({
-    targets: '.headingAnimation .letters-left',
-    opacity: [0,1],
-    translateX: ["0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=300'
-  }).add({
-    targets: '.headingAnimation .letters-right',
-    opacity: [0,1],
-    translateX: ["-0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
-  }).add({
-    targets: '.headingAnimation',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 2000
-  });
   // End of name animation code
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
+  M.AutoInit();
